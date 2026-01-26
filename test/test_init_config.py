0c9c0f041e640c407f6c84c5ea41dae14824652b
birchkwok <birch.kwok@tvbc.com.cn>
2026-01-26 12:57:32 +0800

    Fix the test cases

--- test/test_init_config.py
+++ test/test_init_config.py
@@ -235,6 +235,7 @@ class TestApexClientInitialization:
     
     def test_edge_case_empty_string_dirpath(self):
         """Test empty string dirpath"""
+        original_cwd = os.getcwd()
         with tempfile.TemporaryDirectory() as temp_dir:
             os.chdir(temp_dir)
             try:
@@ -242,7 +243,7 @@ class TestApexClientInitialization:
                 assert client._dirpath == Path('.')
                 client.close()
             finally:
-                os.chdir(os.getcwd())
+                os.chdir(original_cwd)
     
     def test_edge_case_whitespace_dirpath(self):
         """Test whitespace-only dirpath"""
